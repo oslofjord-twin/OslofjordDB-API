7b59929573123cb00f09f91b8d6de78797692281
finstad <andrfins@online.no>
2023-11-07 12:45:54 +0100

    Added important sql file

---
+++ scripts/create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE salinity (
+    record_time timestamp,
+    record_number int,
+    sensor_status varchar(50),
+    conductivity numeric(10, 2),
+    temperature numeric(10, 2)
+);
+
+CREATE TABLE turbidity (
+    record_time timestamp,
+    record_number int,
+    sensor_status varchar(50),
+    turbidity numeric(10, 2),
+    temperature numeric(10, 2),
+    txc_amp numeric(10, 2),
+    c1_amp numeric(10, 2),
+    c2_amp numeric(10, 2),
+    raw_temp numeric(10, 2)
+);
