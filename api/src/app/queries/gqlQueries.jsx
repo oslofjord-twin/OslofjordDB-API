fd84df42c24f51dd20b57dfa95fdba882ef723d9
Sander Sigmundstad <sanderls@icloud.com>
2023-11-17 17:36:00 +0100

    Made the api-web-server run as a docker container

---
+++ api/src/app/queries/gqlQueries.jsx
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client";
+
+export const GET_SALINITY_TIME_FRAME = gql`
+    query Salinity($date_from: timestamptz!, $date_to: timestamptz!) {
+      salinity(where: {record_time: {_gte: ,$date_from _lte: $date_to}}) {
+        record_time
+        temperature
+        conductivity
+      }
+    }
+  `;
+
+export const GET_SALINITY = gql`
+  query Salinity {
+    salinity(limit: 10) {
+      record_time
+      temperature
+      conductivity
+      location
+    }
+  }
+`;
+
+export const GET_TURBDITY = gql`
+  query Turbidity {
+    turbidity(limit: 10) {
+      record_time
+      temperature
+      turbidity
+      location
+    }
+  }
+`;
