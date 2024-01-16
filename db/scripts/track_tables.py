14771aaffe9560f3f7a1a747d5a0cd1137ed2421
Sander Sigmundstad <sanderls@icloud.com>
2024-01-16 16:21:10 +0100

    Added possibility to update schema with comments etc.

--- db/scripts/track_tables.py
+++ db/scripts/track_tables.py
@@ -2,25 +2,36 @@ import requests
 
 url = 'http://host.docker.internal:8080/v1/metadata'
 params = {
-  "type": "bulk",
-  "args": [
-    {
-       "type": "pg_track_table",
-       "args": {
-          "source": "default",
-          "schema": "public",
-          "name": "salinity"
-       }
-    },
-    {
-       "type": "pg_track_table",
-       "args": {
-          "source": "default",
-          "schema": "public",
-          "name": "turbidity"
-       }
-    }
-  ]
+  "type": "pg_track_tables",
+  "args": {
+    "tables": [
+      {
+        "source": "default",
+        "table": "salinity",
+        "configuration": {
+          "column_config": {
+            "conductivity": {
+              "comment": "The conductivity value"
+            }
+          },
+          "comment": "Salinity in the Oslofjord"
+        },
+        "apollo_federation_config": {
+          "enable": "v1"
+        }
+      },
+      {
+        "source": "default",
+        "table": "turbidity",
+        "configuration": {
+            "comment": "Turbidity in the Oslofjord"
+        },
+        "apollo_federation_config": {
+          "enable": "v1"
+        }
+      }
+    ]
+  }
 }
 
 headers = {"Content-Type": "application/json", "X-Hasura-Role": "admin", "X-Hasura-Access-Key": "mylongsecretkey"}
