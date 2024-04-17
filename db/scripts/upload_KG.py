442d1b4eb9147f5ba0823f4d045ccf88d3cdd00f
admin <admin@admin.com>
2024-04-17 15:55:44 +0000

    Fixed script that kept uploading more triples each time we started our containers.

--- db/scripts/upload_KG.py
+++ db/scripts/upload_KG.py
@@ -1,5 +1,12 @@
 import requests
 
+# First delete if fuseki already contains our ontology.
+url = 'http://172.17.0.1:3030/ds/update'
+headers = {'Content-Type': 'application/sparql-update'}
+response = requests.post(url, data="DROP default", headers=headers)
+print(response.content)
+
+# Then upload the new ontology
 url = 'http://172.17.0.1:3030/ds/data'
 data = open('ontology.ttl').read()
 headers = {'Content-Type': 'text/turtle;charset=utf-8'}
