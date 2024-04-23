b2a59afc02968147c7bd9ce410e636ebdf754284
Vebjørn Olsen Leihne <vebjorol@uio.no>
2024-04-23 14:19:49 +0200

    fixed errors in creation of runtime_monitoring and requests tables

--- db/scripts/create_tables.sql
+++ db/scripts/create_tables.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS turbidity (
 CREATE TABLE IF NOT EXISTS runtime_monitoring (
 	id SERIAL PRIMARY KEY,
 	request_id int NOT NULL,
-	id_sim int NOT NULL
+	id_sim int NOT NULL,
 	suitable_temperature boolean,
 	suitable_spawning_temperature boolean,
 	preferred_spawning_temperature boolean
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS requests (
 	request_id SERIAL PRIMARY KEY,
 	grid_id int NOT NULL,
 	species_name TEXT NOT NULL,
-	done boolean NOT NULL DEFAULT 0
+	done boolean NOT NULL DEFAULT false
 );
 
 select create_hypertable('salinity', 'record_time', chunk_time_interval => INTERVAL '1 day');
