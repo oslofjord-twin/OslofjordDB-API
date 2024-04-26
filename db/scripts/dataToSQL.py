f8e89c2903c004b5f652930a4b0e6d396bf26eb4
nrec <nrec@nrec.com>
2024-04-26 14:23:16 +0000

    Testing some changes

--- db/scripts/dataToSQL.py
+++ db/scripts/dataToSQL.py
@@ -3,8 +3,8 @@ import os
 
 
 def csvToSQL():
-    csv_folder = '/mnt/data/csv_files/' 
-    sql_file = '/mnt/data/Oslofjord/OslofjordDB/db/scripts/load_data.sql'
+    csv_folder = './OslofjordDB/db/data' 
+    sql_file = './OslofjordDB/db/scripts/load_data.sql'
 
     table_name = ['turbidity', 'salinity']
     columns = [['record_time', 'record_number', 'sensor_status', 'turbidity', 'temperature', 'txc_amp', 'c1_amp', 'c2_amp', 'raw_temp', 'location'], 
